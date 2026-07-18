23ef5991d7e96e47aebfd6aeac98e3353aa8585f
Khine Ngrein Zan <khinengrinzan@set.p2dpt.tech>
2026-07-18 11:08:13 +0530

    that is update for cafe management system

--- src/main/java/com/mycompany/cafe_management_system/Cafe_Management_System.java
+++ src/main/java/com/mycompany/cafe_management_system/Cafe_Management_System.java
@@ -12,5 +12,6 @@ public class Cafe_Management_System {
 
     public static void main(String[] args) {
         System.out.println("Hello World! cafe cafe cafe cafe");
+        System.err.println("Hello World! Khine Ngrein Zan");
     }
 }
